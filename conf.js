3184353dd13494309133987106d08d31bd3eb75c
unknown <sameena.c.taz@gmail.com>
2020-06-12 23:50:17 +0100

    added new files

--- conf.js
+++ conf.js
@@ -1,4 +1,13 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+    specs: ['repeater.js'],
+    // Capabilities: {
+    //        browserName: 'firefox'
+    // }
+
+    // multiCapabilities: [{
+    //    browserName: 'firefox'
+    //  }, {
+    //    browserName: 'chrome'
+    // }]
 };
